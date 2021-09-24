c12b4057a8e283bcafdde53a17bc517dca2eb5ff
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-24 19:54:58 -0300

    Added Links

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -12,13 +12,21 @@ export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
-        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        <NavLink icon={RiDashboardLine} href="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink icon={RiContactsLine} href="/users">
+          Usuários
+        </NavLink>
       </NavSection>
       <NavSection title="AUTOMAÇÂO">
         <Stack spacing="4" mt="8" align="stretch">
-          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+          <NavLink icon={RiInputMethodLine} href="/forms">
+            Formulários
+          </NavLink>
+          <NavLink icon={RiGitMergeLine} href="/automation">
+            Automação
+          </NavLink>
         </Stack>
       </NavSection>
     </Stack>
