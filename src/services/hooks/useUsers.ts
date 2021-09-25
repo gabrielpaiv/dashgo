f0110de32024e0a16c88824acd7176db40eccbc6
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-25 16:20:47 -0300

    Added Types for useUsers

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,28 +1,33 @@
 import { useQuery } from 'react-query'
 import { api } from '../../services/api'
 
-export function useUsers() {
-  return useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('/users')
+type User = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+}
 
-      const users = data.users.map(user => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          })
-        }
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get('/users')
+
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
       })
-      return users
-    },
-    {
-      staleTime: 1000 * 5
     }
-  )
+  })
+  return users
+}
+
+export function useUsers() {
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5
+  })
 }
