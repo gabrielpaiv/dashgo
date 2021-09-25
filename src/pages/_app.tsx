20078f34d899414626833ab9dbce4aa833f1849d
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-25 15:50:44 -0300

    Added MirageJs and React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { SidebarDrawerProvider } from '../contexts/SideBarDrawerContext'
+import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
